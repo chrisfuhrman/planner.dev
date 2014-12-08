5677b1712da5658f6c54f94a19467cd3b9343ea9
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-12-08 11:26:03 -0600

    initial

---
+++ public/todo_list.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TODO List</title>
+</head>
+<body>
+
+	<h2>TODO List</h2>
+
+	<ul>
+		<li>take out the trash</li>
+		<li>go to the gym</li>
+		<li>take out the trash</li>
+	</ul>
+
+	<h2>Form that adds items to our todo list</h2>
+
+	<form method="POST" action="http://requestb.in/1czyzca1">
+
+		<label for="item">New Task:</label>
+		<input type="item" id="item" name="item">
+<br>
+		<button type="submit">Add</button>
+
+	</form>
+</body>
+</html>
